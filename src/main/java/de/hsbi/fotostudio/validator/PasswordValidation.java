929bdbaafdb79d38a2afbba125a689bac033348c
Frederick <frederick.zahn@fh-bielefeld.de>
2024-06-17 20:59:28 +0200

    Validation vom Passwort funktioniert nicht wegen dem @Injection

--- src/main/java/de/hsbi/fotostudio/validator/PasswordValidation.java
+++ src/main/java/de/hsbi/fotostudio/validator/PasswordValidation.java
@@ -1,18 +1,22 @@
 package de.hsbi.fotostudio.validator;
 
-import de.hsbi.fotostudio.controller.registerCdiBean;
+import de.hsbi.fotostudio.controller.RegisterCdiBean;
 import de.hsbi.fotostudio.util.Util;
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.FacesValidator;
 import jakarta.faces.validator.Validator;
 import jakarta.inject.Inject;
+import java.io.Serializable;
 
-@FacesValidator("inputValidation")
-public class PasswordValidation implements Validator {
+@FacesValidator("passwordValidation")
+@SessionScoped
+public class PasswordValidation implements Validator, Serializable {
+
     @Inject
-    private  registerCdiBean regBean;
-    
+    private RegisterCdiBean regBean;
+
     /**
      * Validates the password. Ensures it meets the minimum length requirement.
      *
@@ -22,17 +26,21 @@ public class PasswordValidation implements Validator {
      */
     @Override
     public void validate(FacesContext fc, UIComponent uic, Object obj) {
-        String wert = obj.toString();
-        String pattern = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}|\\d{5}$";
-        String res = wert.replaceFirst(pattern, "");
-        regBean.setPwdDataOk(false);
-        if (!res.isEmpty()) {
-            // Password does not match pattern, show warning messag
-            Util.addComponentMessage(uic.getClientId(), "warn", "Syntax (Passwort)", "Groß-/Kleinschribung, Sonderzeichen, Zahlen, 8 Zeichen");
-        } else {
-            // Password valid
-            regBean.setPwdDataOk(true);
-            Util.addComponentMessage(uic.getClientId(), "info", "OK!", "OK!");
+        System.out.println("Ich bin hier \n");
+        if (regBean != null) {
+            System.out.println("Ich bin schon wieder Null \n");
+            String wert = obj.toString();
+            String pattern = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}|\\d{5}$";
+            String res = wert.replaceFirst(pattern, "");
+            regBean.setPwdDataOk(false);
+            if (!res.isEmpty()) {
+                // Password does not match pattern, show warning messag
+                Util.addComponentMessage(uic.getClientId(), "warn", "Syntax (Passwort)", "Groß-/Kleinschribung, Sonderzeichen, Zahlen, 8 Zeichen");
+            } else {
+                // Password valid
+                regBean.setPwdDataOk(true);
+                Util.addComponentMessage(uic.getClientId(), "info", "OK!", "OK!");
+            }
         }
     }
 }
